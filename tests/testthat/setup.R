94bdd4a97d8aac90d785a7df9690ee1d82e45dfe
srvander <srvanderplas@gmail.com>
2019-03-20 14:34:04 -0500

    augment checking

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -2,9 +2,9 @@
 url_unreachable <- function(url) {
   opt1 <- try(xml2::read_html(url), silent = T)
   if ("curl" %in% installed.packages()) {
-    res <- curl::curl_fetch_memory(url)
+    res <- try(curl::curl_fetch_memory(url, handle = curl::new_handle(failonerror = T)))
     opt2 <- res$status_code == 200
-    return("try-error" %in% class(opt1) | !opt2)
+    return("try-error" %in% class(opt1) | "try-error" %in% class(opt2))
   } else {
     return("try-error" %in% class(opt1))
   }

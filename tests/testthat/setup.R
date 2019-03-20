a53651b70d3cd5405b39b3f844b8cce04700c666
srvander <srvanderplas@gmail.com>
2019-03-20 14:31:17 -0500

    switch to curl package

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,9 +1,9 @@
 
 url_unreachable <- function(url) {
   opt1 <- try(xml2::read_html(url), silent = T)
-  if ("RCurl" %in% installed.packages()) {
-    opt2 <- RCurl::url.exists(url)
-    
+  if ("curl" %in% installed.packages()) {
+    res <- curl::curl_fetch_memory(url)
+    opt2 <- res$status_code == 200
     return("try-error" %in% class(opt1) | !opt2)
   } else {
     return("try-error" %in% class(opt1))

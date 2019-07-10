d25f8abc5c697571e2b5fa70603f8ac39f590666
Susan Vanderplas <srvanderplas@gmail.com>
2019-07-10 11:35:23 -0500

    Fixing issues with documentation/check

--- R/connect_helper.R
+++ R/connect_helper.R
@@ -1,31 +1,33 @@
 #' Set up connection to NBTRD
-#' 
+#'
 #' This function connects to a pre-existing Docker firefox container running
 #' on localhost:4445, and if none is found, it uses the RSelenium client option,
 #' (which is deprecated) to connect.
-#' 
+#'
 #' @param strict error if RSelenium connection does not succeed?
 #' @return RSelenium remoteDriver object, if connection was successful, NULL otherwise.
 #' @export
 setup_NBTRD <- setup_NRBTD <- function(strict = T) {
-  
-  # Docker option 
-  remDr <- RSelenium::remoteDriver(remoteServerAddr = "localhost", port = 4445L, 
-                                   browserName = "firefox")
+
+  # Docker option
+  remDr <- RSelenium::remoteDriver(
+    remoteServerAddr = "localhost", port = 4445L,
+    browserName = "firefox"
+  )
   remDr$open()
   remDr$navigate(url = "https://tsapps.nist.gov/NRBTD/")
-  
-  
+
+
   if (!remDr$getStatus()$ready) {
     # Client option
     rD <- RSelenium::rsDriver(browser = "firefox")
     remDr <- rD[["client"]]
   }
-  
-  
+
+
   # Establish a wait for an element
   # remDr$setImplicitWaitTimeout(1000)
-  
+
   if (remDr$getStatus()$ready) {
     return(remDr)
   } else {
@@ -34,7 +36,7 @@ setup_NBTRD <- setup_NRBTD <- function(strict = T) {
     } else {
       warning("Could not connect to Selenium server via Docker or Client")
     }
-    
+
     return(NULL)
   }
 }

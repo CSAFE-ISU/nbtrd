ed396a360f91fb98b684a5ca3c9d6e8083fa14ee
Susan Vanderplas <srvanderplas@gmail.com>
2019-08-06 15:52:43 -0500

    make executable, add cron ping that the job ran

--- inst/PingNIST.R
+++ inst/PingNIST.R
@@ -9,6 +9,8 @@ library(XML)
 if (!exists("nbtrd_user")) nbtrd_user <- "srvander"
 if (!exists("nbtrd_pwd")) nbtrd_pwd <- keyringr::decrypt_gk_pw("db csafe user srvander")
 
+cron_ran <- httr::GET("https://hc-ping.com/b974ffc9-ff32-432d-a454-fbf76b278de8")
+
 remDr <- setup_NBTRD()
 
 if (str_detect(remDr$getCurrentUrl()[[1]], "nist.gov")) {

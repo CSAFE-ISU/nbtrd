8785a51a328d84b279d27e11f43c9303de5ce8d4
Susan Vanderplas <srvanderplas@gmail.com>
2019-12-11 15:25:02 -0600

    Add port number, change selenium syntax to modern version. Remove local client possibility - too unstable.

--- R/connect_helper.R
+++ R/connect_helper.R
@@ -4,31 +4,32 @@
 #' on localhost:4445, and if none is found, it uses the RSelenium client option,
 #' (which is deprecated) to connect.
 #'
+#' @param selenium_port integer port number of docker container
 #' @param strict error if RSelenium connection does not succeed?
 #' @return RSelenium remoteDriver object, if connection was successful, NULL otherwise.
 #' @export
-setup_NBTRD <- setup_NRBTD <- function(strict = T) {
+setup_NBTRD <- setup_NRBTD <- function(selenium_port = 4445L, strict = T) {
 
   # Docker option
   remDr <- RSelenium::remoteDriver(
-    remoteServerAddr = "localhost", port = 4445L,
+    remoteServerAddr = "localhost", port = selenium_port,
     browserName = "firefox"
   )
   remDr$open()
   remDr$navigate(url = "https://tsapps.nist.gov/NRBTD/")
 
-
-  if (!remDr$getStatus()$ready) {
-    # Client option
-    rD <- RSelenium::rsDriver(browser = "firefox")
-    remDr <- rD[["client"]]
-  }
+# 
+#   if (!remDr$getStatus()$ready) {
+#     # Client option
+#     rD <- RSelenium::rsDriver(browser = "firefox")
+#     remDr <- rD[["client"]]
+#   }
 
 
   # Establish a wait for an element
   # remDr$setImplicitWaitTimeout(1000)
 
-  if (remDr$getStatus()$ready) {
+  if (length(remDr$getSession()) > 0) {
     return(remDr)
   } else {
     if (strict) {
